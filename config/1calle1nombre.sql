01b966b8f04620143834be4b31d06b5aacddf016
Hugoren Martinako <aumpfbahn@gmail.com>
2021-03-05 20:43:57 +0100

    add prov/ccaa ids to the reports

--- config/1calle1nombre.sql
+++ config/1calle1nombre.sql
@@ -1,31 +1,92 @@
 SELECT
   results."ine:municipio" AS id
+  , results.prov
+  , results.ccaa
   , results.name
   , current_date AS date
   , results.incomplete
   , results.total
-  , CASE WHEN results.total = 0 THEN 1 ELSE round( 1-( results.incomplete::NUMERIC / results.total ) , 2 ) END AS percentage
+  ,
+  CASE
+    WHEN results.total = 0 THEN 1
+    ELSE round( 1-( results.incomplete::NUMERIC / results.total ) , 2 )
+  END AS percentage
   , round(results.length_incomplete::NUMERIC, 2) AS length_incomplete
   , round(results.length_total::NUMERIC, 2) AS length_total
-  , CASE WHEN results.length_total = 0 THEN 1 ELSE round((1-( results.length_incomplete / results.length_total ))::NUMERIC, 2) END AS length_percentage
+  ,
+  CASE
+    WHEN results.length_total = 0 THEN 1
+    ELSE round((1-( results.length_incomplete / results.length_total ))::NUMERIC, 2)
+  END AS length_percentage
 FROM
   (
+    WITH prov AS (
+      SELECT
+        "ISO3166-2" AS id
+        , way
+      FROM
+        planet_osm_polygon
+      WHERE
+        admin_level = '6'
+        AND boundary = 'administrative'
+    )
+    , ccaa AS (
+      SELECT
+        "ISO3166-2" AS id
+        , way
+      FROM
+        planet_osm_polygon
+      WHERE
+        admin_level = '4'
+        AND boundary = 'administrative'
+    )
     SELECT
       municipios."ine:municipio"
+      , (
+        SELECT
+          id
+        FROM
+          prov
+        WHERE
+          st_within(
+            municipios.way
+            , prov.way
+          )
+      ) AS prov
+      , (
+        SELECT
+          id
+        FROM
+          ccaa
+        WHERE
+          st_within(
+            municipios.way
+            , ccaa.way
+          )
+      ) AS ccaa
       , municipios.name
-      , sum(CASE WHEN (pol.highway IN ('residential', 'living_street', 'pedestrian') AND pol.name IS NULL AND pol.noname IS NULL) THEN 1 ELSE 0 END) AS incomplete
-      , sum(CASE WHEN (pol.highway IN ('residential', 'living_street', 'pedestrian')) THEN 1 ELSE 0 END) AS total
-      , sum(CASE WHEN (pol.highway IN ('residential', 'living_street', 'pedestrian') AND pol.name IS NULL AND pol.noname IS NULL) THEN st_length(pol.way, true) ELSE 0 END) AS length_incomplete
-      , sum(CASE WHEN (pol.highway IN ('residential', 'living_street', 'pedestrian')) THEN st_length(pol.way, true) ELSE 0 END) AS length_total
+      , sum(CASE WHEN (pol.name IS NULL AND pol.noname IS NULL) THEN 1 ELSE 0 END) AS incomplete
+      , sum(1) AS total
+      , sum(CASE WHEN (pol.name IS NULL AND pol.noname IS NULL) THEN st_length(pol.way, TRUE) ELSE 0 END) AS length_incomplete
+      , sum(st_length(pol.way, TRUE)) AS length_total
     FROM
-      planet_osm_line pol
+      (
+        SELECT
+          *
+        FROM
+          planet_osm_line
+        WHERE
+          highway IN (
+            'residential', 'living_street', 'pedestrian'
+          )
+      ) AS pol
       , (
         SELECT
           name
           , way
           , COALESCE("ine:municipio", osm_id::TEXT) AS "ine:municipio"
         FROM
-          planet_osm_polygon
+          planet_osm_polygon pop
         WHERE
           admin_level = '8'
           AND boundary = 'administrative'
@@ -62,4 +123,5 @@ FROM
     GROUP BY
       municipios."ine:municipio"
       , municipios.name
+      , municipios.way
   ) AS results
